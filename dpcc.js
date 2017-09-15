eab511397f2b220fa398f0e2f005a28d37237663
Peter Eichman <peichman@umd.edu>
2017-09-15 11:17:14 -0400

    Created get_costs function.
    Moved the calculations inside of a function that returns a "costs" object with
    four keys: ingest, storage, yearly, and total.

--- dpcc.js
+++ dpcc.js
@@ -4,28 +4,41 @@ function non_negative(x) {
     return x >= 0 ? x : 0;
 }
 
-// rates
-var ingest_rate = 310;
-var membership_fee = 2750;
-var storage_rate = 500;
+function get_costs(service, amount_stored, time) {
+    var storage_used = non_negative(amount_stored - service.included_storage);
+    var storage_required = Math.ceil(storage_used/service.storage_increment) * service.storage_increment;
+    var storage_cost = service.storage_rate * storage_required;
+    var yearly_cost = storage_cost + service.membership_fee;
 
-// variables
-var included_storage = 0;
-var storage_increment = 1;
+    var ingest_cost = service.ingest_rate * amount_stored;
+    var total_cost = (time * yearly_cost) + ingest_cost;
+
+    return {
+        ingest: ingest_cost,
+        storage: storage_cost,
+        yearly: yearly_cost,
+        total: total_cost,
+    };
+}
+
+var service = {
+    // rates
+    ingest_rate: 310,
+    membership_fee: 2750,
+    storage_rate: 500,
+    // variables
+    included_storage: 0,
+    storage_increment: 1,
+};
 
 // user-supplied
 var amount_stored = 50;
 var time = 5;
 
-var storage_used = non_negative(amount_stored - included_storage);
+// results
+var costs = get_costs(service, amount_stored, time);
 
-var storage_cost = storage_rate * Math.ceil(storage_used/storage_increment) * storage_increment;
-var yearly_cost = storage_cost + membership_fee;
-
-var ingest_cost = ingest_rate * amount_stored;
-var total_cost = (time * yearly_cost) + ingest_cost;
-
-console.log(storage_cost);
-console.log(yearly_cost);
-console.log(ingest_cost);
-console.log(total_cost);
+console.log('Storage cost ($/y): ' + costs.storage);
+console.log('   Yearly cost ($): ' + costs.yearly);
+console.log('   Ingest cost ($): ' + costs.ingest);
+console.log('    Total cost ($): ' + costs.total);
